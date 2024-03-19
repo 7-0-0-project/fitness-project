6faad9583ec26c69dfc9a19048a7d550aecfd5ea
Jordy <jordy.lopez2003@gmail.com>
2024-03-19 15:05:05 -0400

    committed

--- fitness-app/src/App.jsx
+++ fitness-app/src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import API_KEY from './config'
+import { handleFetch } from './utils';
 
 function App() {
 
-  //try it
+  const [data, setData] = useState([]);
+  const [error, setError] = useState('');
+
+  //fetch data
+  useEffect(() => {
+    const doFetch = async () => {
+      const headers = {
+        'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+        'x-rapidapi-key': API_KEY
+      }
+      const [data, error] = await handleFetch(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=10`)
+      console.log(data);
+      if (data) setData(data);
+      if (error) setError(error)
+    }
+    doFetch();
+  }, []); //empty array will run only once 
+
+  //code to render the data or the error
+  console.log(data);
+
   return (
     <>
 
---
+++ fitness-app/src/config.js
@@ -0,0 +1,3 @@
+const API_KEY = "f7aa9db4a9msh5569ebba3d783e8p13ac4ajsn6b01bcb78f5e";
+
+export default API_KEY;
