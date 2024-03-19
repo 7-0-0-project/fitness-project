25f58dd757d37e0e0b3c1ff59675dd63e7622ec9
xhenis <xhenism2001@gmail.com>
2024-03-19 15:01:54 -0400

    Nav bar with working routing. Needs CSSgit add -A

--- fitness-app/src/App.jsx
+++ fitness-app/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './components/nav-bar';
+import Home from './pages/Home';
+import About from './pages/about';
+import FitnessCatalog from './pages/fitness-catalog';
 function App() {
-
-  //try it
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/fitness-catalog' element={<FitnessCatalog />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App
---
+++ fitness-app/src/pages/fitness-catalog.jsx
@@ -0,0 +1,3 @@
+export default function FitnessCatalog() {
+    return <h1>FitnessCatalog</h1>
+}
