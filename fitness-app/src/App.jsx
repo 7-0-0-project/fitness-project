148653457785ff81f6f868bae9c4faba5a15d4ad
Jordy <jordy.lopez2003@gmail.com>
2024-03-19 16:06:35 -0400

    progress

--- fitness-app/src/App.jsx
+++ fitness-app/src/App.jsx
@@ -5,29 +5,51 @@ import { handleFetch } from './utils';
 
 function App() {
 
+
+
   const [data, setData] = useState([]);
   const [error, setError] = useState('');
 
   //fetch data
   useEffect(() => {
     const doFetch = async () => {
-      const headers = {
-        'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-        'x-rapidapi-key': API_KEY
+      const url =
+        'https://exercisedb.p.rapidapi.com/exercises/bodyPartList';
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "36dd3cd46dmsheb990f286d91c4ap105ac5jsne5812b52a8d9", // Use your actual API key
+          "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result)
+        // Trigger download
+        // if (result.message === '400') return
+        // const blob = new Blob([JSON.stringify(result, null, 2)], { type: 'application/json' });
+        // const href = URL.createObjectURL(blob);
+        // const link = document.createElement('a');
+        // link.href = href;
+        // link.download = "myExercises.json"; // or any other filename
+        // document.body.appendChild(link);
+        // link.click();
+        // document.body.removeChild(link);
+      } catch (error) {
+        console.error(error);
       }
-      const [data, error] = await handleFetch(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=10`)
-      console.log(data);
-      if (data) setData(data);
-      if (error) setError(error)
-    }
+    };
+
     doFetch();
-  }, []); //empty array will run only once 
+  }, []);
 
-  //code to render the data or the error
   console.log(data);
-
   return (
     <>
+
 
     </>
   )
