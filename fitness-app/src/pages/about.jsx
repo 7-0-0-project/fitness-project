b73798ceb6a31ad95b6b5e2a4b633c8f4c107283
xhenis <xhenism2001@gmail.com>
2024-03-31 15:41:34 -0400

    pushing

--- fitness-app/src/pages/about.jsx
+++ fitness-app/src/pages/about.jsx
@@ -1,3 +1,15 @@
-export default function About() {
-    return <h1>About</h1>
+import React from "react"
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <img className="goku-img" src="https://www.superherotoystore.com/cdn/shop/articles/e33c2fa94c03efa06678116f80d62d0d_708x.jpg?v=1590599656" alt="Goku" />
+            <h3>Goku Approved</h3>
+
+        </div>
+    )
+
 }
+
+
+export default About
